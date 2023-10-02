920881068eac82e666dcb0a6a34beb1809f5a5fe
Marcela Mejia <mejiao24@hotmail.com>
2023-10-02 19:05:08 +1100

    Add basic styling to the product page

--- src/pages/PlantPage/PlantPage.jsx
+++ src/pages/PlantPage/PlantPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import { getPlantById } from '../../services/items-services';
+import styles from '../PlantPage/PlantPage.module.scss';
 
 const PlantMovie = () => {
 	const { id } = useParams();
@@ -15,17 +16,17 @@ const PlantMovie = () => {
 
 
 	return (
-		<main>
+		<main className={styles.container}>
 			{plant && (
-				<>
+				<div className={styles.card}>
 					<h1>{plant.name}</h1>
-					<img src={plant.imageLink} alt={plant.name} />
+					<img className={styles.image} src={plant.imageLink} alt={plant.name} />
 					<p>Price: ${plant.price}</p>
 					<ul>Colors: 
 						{plant.variants.map((variant, index) => <li key={index}>{variant.color}</li>
 						)}
 					</ul>
-				</>
+				</div>
 			)}
 			{error && <p>Could not find plant with Id: {id}</p>}
 		</main>
