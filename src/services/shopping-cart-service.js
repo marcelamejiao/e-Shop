40b32d83957790f7baf653ecd3c95be0936b35ef
Marcela Mejia <mejiao24@hotmail.com>
2023-10-04 21:00:16 +1100

    implement shopping-cart service

---
+++ src/services/shopping-cart-service.js
@@ -0,0 +1,22 @@
+import {
+	doc,
+	setDoc
+} from 'firebase/firestore';
+import { db } from '../config/firestore';
+
+const shoppingCartId = '1'
+
+export const updateShoppingCart = async (data) => {
+	try {
+		await setDoc(
+			doc(db, 'shopping-cart', shoppingCartId),
+			{
+				...data,
+				quantity: 1
+			}
+			)
+	} catch (e) {
+		console.log(e);
+    throw e;
+	}
+}
