7fbccd96558102e7f23a3175ee5cb4d29c084d29
Marcela Mejia <mejiao24@hotmail.com>
2023-10-07 23:59:08 +1100

    implement carousel functionality

--- src/component/CarouselItem/CarouselItem.jsx
+++ src/component/CarouselItem/CarouselItem.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 const CarouselItem = ({ plant }) => {
 	return (
-	<article>
+	<article style={{height: '200px', width: '100px'}}>
 			<h2>{plant.name}</h2>
-			<img src={plant.imageLink} alt={plant.name} />
+			<img style={{height: '100px', width: '50px'}} src={plant.imageLink} alt={plant.name} />
 			<p>Price: ${plant.price}</p>
 			<Link to={plant.id}>Open this Item</Link>
 	</article>
--- src/container/Carousel/Carousel.jsx
+++ src/container/Carousel/Carousel.jsx
@@ -1,14 +1,49 @@
 import CarouselItem from '../../component/CarouselItem/CarouselItem';
+import { useState } from 'react';
 
 const Carousel = ({ plants }) => {
+	const [currentIndex, setCurrentIndex] = useState(0);
+
+	let maxSlidesIndex = 0;
+
+	const previousSlide = () => {
+		// when press the back button on first slide, go to the last slide
+		if(currentIndex === 0) {
+			setCurrentIndex(maxSlidesIndex);
+		// otherwise go to previous slide
+		} else {
+			setCurrentIndex(currentIndex - 1);
+		}
+	}
+
+	const nextSlide = () => {
+		// when press the next button on last slide, go to the first slide
+		if(currentIndex === maxSlidesIndex) {
+			setCurrentIndex(0);
+		// otherwise go to next slide
+		} else {
+			setCurrentIndex(currentIndex + 1);
+		}
+	}
+
 	return (
 		<>
 			<h2>Top Sellers:</h2>
-			{
-				plants
-					.filter((plant) => plant.favourited)
-					.map((plant, index) => <CarouselItem key={index} plant={plant} />)
-			}
+			<div style={{display: 'flex'}}>
+				<button onClick={previousSlide}>&#10094;</button>
+				{
+					plants
+						.filter((plant) => plant.favourited)
+						.map((plant, index) => {
+							// store the last index of the favourited plants
+							maxSlidesIndex = index;
+							if(index === currentIndex) {
+								return <CarouselItem key={index} plant={plant} />
+							}
+						})
+				}
+				<button onClick={nextSlide}>&#10095;</button>
+			</div>
 		</>
 	);
 }
